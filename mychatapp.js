9c4216e171fce8d67c46c59c5a80125615c3e0b1
Ozgur Ugurlu <ozguru@Ozgur-Ugurlu.local>
2018-05-01 13:57:23 -0700

    update keys

--- mychatapp.js
+++ mychatapp.js
@@ -1,69 +1,48 @@
-var ChatEngineInitParams = [
-  {
-    publishKey: 'pub-c-31365c8b-8128-4c58-8fc8-ca26b3b6a138',
-    subscribeKey: 'sub-c-6be8075c-c413-11e7-ba65-4295fab41076'
-  }, {
-    // debug: true,
-      globalChannel: 'global-tester2',
-      endpoint: 'https://pubsub.pubnub.com/v1/blocks/sub-key/sub-c-73eb70de-c3f4-11e7-b375-767e250a4e65/server'
-  }
-];
-
+ChatEngine = ChatEngineCore.create({
+    publishKey: 'pub-c-fab5d74d-8118-444c-b652-4a8ee0beee92',
+    subscribeKey: 'sub-c-696d9116-c668-11e7-afd4-56ea5891403c'
+});
+// To keep things clean we'll put all our code in the app object.
 var app = {
-    messageToSend: '',
-    ChatEngine: false,
-    me: false,
-    chat: false,
-    users: {},
-    messages: [],
+    users: {},      // An object to store our users in
+    messages: [],   // A list to store our messages in
     init: function() {
-        // Make sure to import ChatEngine first!
-        this.ChatEngine = ChatEngineCore.create({
-            publishKey: 'pub-c-31365c8b-8128-4c58-8fc8-ca26b3b6a138',
-            subscribeKey: 'sub-c-6be8075c-c413-11e7-ba65-4295fab41076'
-        });
+        // This is a helper method to generate a random chat profile
+        let newPerson = generatePerson(true);
+      
+        ChatEngine.connect(newPerson.uuid, newPerson);
 
-        // Let's remember our random generated users :)
-        var myChatUser = JSON.parse(localStorage.getItem("myChatUser"));
-        if (!myChatUser) {
-            myChatUser = generatePerson(true);
-            localStorage.setItem('myChatUser', JSON.stringify(myChatUser));
-        }
-        
-        this.ChatEngine.connect(myChatUser.uuid, myChatUser, 'auth-key');
-
-        this.ChatEngine.on('$.ready', function(data) {
-
+        ChatEngine.on('$.ready', function(data) {
             app.me = data.me;
-            app.chat = new app.ChatEngine.Chat('chat-engine-demo2');
+            app.myFirstChatRoom = new ChatEngine.Chat('chatengine-demo-room');
+
+            // Listen for messages.
+            app.myFirstChatRoom.on('message', function(message) {
+                app.renderMessage(message);
+            });
             
             // Listen for users coming online
-            // Presence Joins and Herenows
-            app.chat.on('$.online.*', function(data) {
+            app.myFirstChatRoom.on('$.online.*', function(data) {
                 app.users[data.user.uuid] = data.user;
                 app.renderUsers();
             });
 
             // Listen for users going offline
-            app.chat.on('$.offline.*', function(data) {
+            app.myFirstChatRoom.on('$.offline.*', function(data) {
                 delete app.users[data.user.uuid];
                 app.renderUsers();
             });
-
-            // Listen for messages.
-            app.chat.on('message', function(message) {
-                console.log(message)
-                app.renderMessage(message);
-            });
-
-            let searchy = app.chat.search({
-              reverse: true,
-              event: 'message',
-              limit: 50
-            });
-
-            searchy.on('message', (data) => {
-              app.renderMessage(data);
+          
+            // Show history of your channel as soon as it's connected
+            app.myFirstChatRoom.on('$.connected', function() {
+               
+                app.myFirstChatRoom.search({
+                  reverse: true,
+                  event: 'message',
+                  limit: 50
+                }).on('message', (data) => {
+                  app.renderMessage(data);
+                });
             });
 
         });
@@ -71,30 +50,28 @@ var app = {
     },
 
     sendMessage: function() {
-        this.messageToSend = this.$textarea.val()
-        if (this.messageToSend.trim() !== '') {
-            this.$textarea.val('');
-            this.chat.emit('message', {
-                text: this.messageToSend
+        if ($('#message-to-send').val().trim().length != 0) {
+
+            this.myFirstChatRoom.emit('message', {
+                text: $('#message-to-send').val().trim()
             });
+
+            $('#message-to-send').val('');
         }
     },
     
-    cacheDOM: function() {
-        this.$chatHistory = $('.chat-history');
-        this.$button = $('button');
-        this.$textarea = $('#message-to-send');
-        this.$chatHistoryList = this.$chatHistory.find('ul');
-    },
     bindEvents: function() {
-        this.$button.on('click', this.sendMessage.bind(this));
-        this.$textarea.on('keyup', this.sendMessageEnter.bind(this));
-
+        $('button').on('click', this.sendMessage.bind(this));
+        $('#message-to-send').on('keyup', (event)=> {
+            // enter was pressed
+            if (event.keyCode === 13) {
+                this.sendMessage();
+            }
+        });
     },
-    renderUsers: function() {
 
+    renderUsers: function() {
         var peopleTemplate = Handlebars.compile($("#person-template").html());
-        var user = false;
         $('#people-list ul').empty();
         for (const [ uuid, user ] of Object.entries(app.users)) {
             $('#people-list ul').append(peopleTemplate(user.state));                
@@ -107,11 +84,10 @@ var app = {
         var userTemp = Handlebars.compile($("#message-response-template").html());
 
         var template = userTemp;
+
         if (message.sender.uuid == app.me.uuid) {
             template = meTemp;
         }
-
-        console.log(message)
 
         var context = {
             messageOutput: message.data.text,
@@ -119,26 +95,20 @@ var app = {
             user: message.sender.state
         };
 
-        app.$chatHistoryList.append(template(context));
+        $('.chat-history').find('ul').append(template(context));
 
         this.scrollToBottom();
 
     },
 
-    sendMessageEnter: function(event) {
-        // enter was pressed
-        if (event.keyCode === 13) {
-            this.sendMessage();
-        }
-    },
     scrollToBottom: function() {
-        this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
+        $('.chat-history').scrollTop($('.chat-history')[0].scrollHeight);
     },
     getCurrentTime: function() {
         return new Date().toLocaleTimeString().
         replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
     },
 };
-app.cacheDOM();
+
 app.bindEvents();
 app.init();
